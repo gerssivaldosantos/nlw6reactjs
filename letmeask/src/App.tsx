d50463f1d504ce8b6323bdb5468ef009f9991d0f
gerssivaldosantos <canalandows@gmail.com>
2021-10-11 00:21:00 -0300

    props components example

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,7 +1,14 @@
+import {Button} from './components/button'
 
 function App() {
   return (
+    <div>    
+      
     <h1>Hello World</h1>
+    <Button text="Hello World"/>
+    <Button text="Example two"/>
+    </div>
+
   );
 }
 
