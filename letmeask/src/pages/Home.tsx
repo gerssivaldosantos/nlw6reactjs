f24110441f3888c11ddce2fd52f187a3f3cc425a
gerssivaldosantos <canalandows@gmail.com>
2021-10-12 11:20:02 -0300

    google autentication

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -1,6 +1,8 @@
 
 import { useHistory } from 'react-router-dom'
 
+import {auth, firebase} from '../services/firebase'
+
 import ilustrationImg from '../assets/illustration.svg'
 import logoImg from '../assets/logo.svg'
 import googleIconImg from '../assets/google-icon.svg'
@@ -12,9 +14,19 @@ import { Button } from '../components/button'
 export function Home(){
 
     const history = useHistory();
-    function NavigateToNewRoom(){
-        history.push('/rooms/new')
+
+    function handleCreateRoom(){
+        const provider = new firebase.auth.GoogleAuthProvider()
+
+        auth.signInWithPopup(provider).then(result => {
+            console.log(result)
+            history.push('/rooms/new')
+        })
+
+    
     }
+
+    
     return(
         <div id="page-auth">
             <aside>
@@ -25,7 +37,7 @@ export function Home(){
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="letmeask" />
-                    <button onClick={NavigateToNewRoom} className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg}  alt="Logo da google"/>
                         Crie sua sala com o Google
                     </button>
--- letmeask/src/services/firebase.ts
+++ letmeask/src/services/firebase.ts
@@ -1,7 +1,9 @@
-import 'firebase/database'
-import 'firebase/auth'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+import 'firebase/compat/firestore';
 
-import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -12,4 +14,9 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_MESSAGING_APP_ID 
 };
 
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+const auth =  firebase.auth();
+const database = firebase.database()
+
+export {firebase, auth, database}
