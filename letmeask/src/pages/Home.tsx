47739e0b40118f6bb3a1b86701c1e00067cd53ca
gerssivaldosantos <canalandows@gmail.com>
2021-10-13 09:36:27 -0300

    google autentication with context information shared

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -1,55 +1,58 @@
+import { useContext } from 'react'
 
 import { useHistory } from 'react-router-dom'
 
-import {auth, firebase} from '../services/firebase'
+import { auth, firebase } from '../services/firebase'
 
 import ilustrationImg from '../assets/illustration.svg'
 import logoImg from '../assets/logo.svg'
 import googleIconImg from '../assets/google-icon.svg'
 
+import { AuthContext } from '../App'
+
 import '../styles/auth.scss'
 
 import { Button } from '../components/button'
 
-export function Home(){
+export function Home() {
 
     const history = useHistory();
 
-    function handleCreateRoom(){
-        const provider = new firebase.auth.GoogleAuthProvider()
+    const { user, signInWithGoogle } = useContext(AuthContext)
 
-        auth.signInWithPopup(provider).then(result => {
-            console.log(result)
+    function handleCreateRoom() {
+        signInWithGoogle()
+        if (user !== undefined) {
             history.push('/rooms/new')
-        })
-
-    
+            console.log(user)
+        }
     }
 
-    
-    return(
+    return (
         <div id="page-auth">
+
             <aside>
                 <img src={ilustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
                 <strong>Crie salas de Q&amp;A ao vivo</strong>
                 <p>Tire as dúvidas da sua adiência em tempo real</p>
             </aside>
             <main>
+                {console.log(user)}
                 <div className="main-content">
                     <img src={logoImg} alt="letmeask" />
                     <button onClick={handleCreateRoom} className="create-room">
-                        <img src={googleIconImg}  alt="Logo da google"/>
+                        <img src={googleIconImg} alt="Logo da google" />
                         Crie sua sala com o Google
                     </button>
                     <div className="separator">Ou entre em uma sala</div>
                     <form action="">
-                        <input 
+                        <input
                             type="text"
                             placeholder="Digite o código da sala" />
                         <Button type="submit">
                             Entrar na sala
                         </Button>
-                      
+
                     </form>
                 </div>
             </main>
